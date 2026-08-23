d4ca88198bc871d212c8b06cc170aea5831c3497
amohamedpsaad-a11y <amohamedpsaad@gmail.com>
2026-08-23 05:48:12 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -723,7 +723,7 @@ function handleSuccessPage(url, env) {
 </div>
 <script>
 (function(){
-  const EXTENSION_ID = "hkogoheijgpicaokfbdpehgppdgppijl";
+  const EXTENSION_ID = "naneplpkjaopmhgfnlilahhinigppnna";
   const checkoutId = ${JSON.stringify(checkoutId)};
 
   function openDashboard() {
